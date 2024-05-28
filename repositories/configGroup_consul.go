556fed38bbe922a65ca78bf0cf2bc9e403dd40a3
Marko Čuturilo <markocuturilo001@gmail.com>
2024-05-28 23:54:58 +0200

    Config Consul fix

--- repositories/configGroup_consul.go
+++ repositories/configGroup_consul.go
@@ -28,23 +28,23 @@ func NewConfigGroupConsulRepository() (model.ConfigGroupRepository, error) {
 func (r *ConfigGroupConsulRepository) AddGroup(configGroup model.ConfigGroup) error {
     kv := r.client.KV()
 
-    // Marshal the configGroup into JSON
+
     p, err := json.Marshal(configGroup)
     if err != nil {
-        // Handle the error, such as logging or other error handling mechanism
+
         return err
     }
 
-    // Create a KVPair
+
     kvp := &api.KVPair{
         Key:   fmt.Sprintf("configgroup/%s/%d", configGroup.Name, configGroup.Version),
         Value: p,
     }
 
-    // Put the KVPair into Consul KV store
+
     _, err = kv.Put(kvp, nil)
     if err != nil {
-        // Handle the error, such as logging or other error handling mechanism
+
         return err
     }
 
@@ -121,24 +121,23 @@ LabelLoop:
 
 
 func (r *ConfigGroupConsulRepository) DeleteConfigInListByLabels(name string, version int, labels []model.ConfigInList) error {
-	// Get the configGroup from the repository
+
 	configGroup, err := r.GetGroup(name, version)
 	if err != nil {
 		return err
 	}
 
-	// Iterate over the configInList and remove those that match the provided labels
 	var updatedConfigInList []model.ConfigInList
 LabelLoop:
 	for _, configInList := range configGroup.ConfigInList {
 
-		// Check if the number of labels in the configInList matches the number of provided labels
+
 		if len(configInList.Labels) != len(labels) {
 			updatedConfigInList = append(updatedConfigInList, configInList)
 			continue
 		}
 
-		// Iterate over the provided labels and compare them with the labels in the configInList
+
 		for _, providedLabel := range labels {
 			found := false
 			for labelName, labelValue := range configInList.Labels {
@@ -154,7 +153,7 @@ LabelLoop:
 		}
 	}
 
-	// Update the configGroup in the Consul repository
+
 	configGroup.ConfigInList = updatedConfigInList
 	err = r.AddGroup(configGroup)
 	if err != nil {
